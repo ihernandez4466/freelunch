b6fb0979f46b50f72107588bbcb46fd67856ff13
isabel.hernandez <isabel.hernandez@omron.com>
2023-05-06 16:32:56 -0700

    Migration to react

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <header>
+          <h1>
+            Free Lunch Co
+          </h1>
+        </header>
+      </body>
+    </div>
+  );
+}
+
+export default App;
