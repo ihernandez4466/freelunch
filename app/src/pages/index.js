870c53f712bc3c125e6a48e581290ca34cfdced3
Isabel Hernandez <ihernandez44@csuchico.edu>
2023-10-02 23:06:00 -0700

    This one also

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,17 +1,36 @@
 'use client'
-import Button from '@mui/material/Button';
-import { signIn } from "next-auth/react";
-import { signOut } from "next-auth/react";
+//import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { red, pink } from '@mui/material/colors';
+import { CustomNavBar } from '../components/navbar';
  
 function Home() {
   return (
-    <div className="App">
-        <h1>
-          Free Lunch Co
-        </h1>
-        <Button onClick={() => signIn("google")} variant="contained">Login</Button>
-        <Button onClick={() => signOut()}>Sign out</Button>
-    </div>
+      <Box sx={{ 
+          flexGrow: 1, 
+          height: '100%',
+          width: '100%', 
+        }}>
+        <CustomNavBar/>
+        <Grid
+            container
+            direction="column"
+            justifyContent="space-between"
+            alignItems="stretch"
+            sx={{backgroundColor: pink[200]}}
+        >
+          <Grid item xs={12}>
+              <button>Hola</button>
+          </Grid>
+          <Grid item md={12}>
+              <button>Como Te Llamas</button>
+          </Grid>
+          <Grid item xs={12}>
+              <button>Espero que esto funcione</button>
+          </Grid>
+        </Grid>
+      </Box>
   );
 }
 
