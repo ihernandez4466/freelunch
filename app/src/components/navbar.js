ef1e8b258d9946daf834e8192074439ee9f8bf49
Isabel Hernandez <ihernandez44@csuchico.edu>
2023-12-04 19:04:43 -0800

    Design mouse in mouse out stable - just need to fix css

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,11 +1,8 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-//import Link from 'next/link';
 import Image from 'react-bootstrap/Image';
 import { Link as ScrollLink } from 'react-scroll';
-import { useState } from "react";
-import { MagicTabSelect } from "react-magic-motion";
 
 export function CustomNavBar() {
     return (
@@ -15,69 +12,81 @@ export function CustomNavBar() {
         style={{ justifyContent: 'space-between', display: 'flex' }}>
         <Navbar.Brand>
             <Image 
-              src="http://localhost:3000/images/logo-icon.png"
+              src={process.env.REACT_APP_HOST + '/images/branding/logo-icon.png'}
               style={{
-              height:'50px'}}
+              height:'50px',
+            }}
               ></Image>
         </Navbar.Brand>
-        <Nav >
-          <UnderlineTabs />
+        <Nav variant="underline" >
+          <ScrollLink activeClass="active" to="home" spy={true} smooth={true} duration={300} offset={-50} style={{ color: 'var(--textPrimary)', fontSize: '25px'}}>
+            Home
+          </ScrollLink>
+          <ScrollLink activeClass="active" to="designs" spy={true} smooth={true} duration={300} offset={-50} style={{ color: 'var(--textPrimary)', fontSize: '25px'}}>
+            Designs
+          </ScrollLink>
+          <ScrollLink activeClass="active" to="about-us" spy={true} smooth={true} duration={300} offset={-50} style={{ color: 'var(--textPrimary)', fontSize: '25px'}}>
+            About Us
+          </ScrollLink>
+          <ScrollLink activeClass="active" to="contact-us" spy={true} smooth={true} duration={300} offset={-50} style={{ color: 'var(--textPrimary)', fontSize: '25px' }}>
+            Contact Us
+          </ScrollLink>
         </Nav>
       </Container>
     </Navbar>
     );
 }
-const underlineTabs = [{
-  text: 'Home',
-  to: 'home',
-}, {
-  text: 'Designs & Apparel',
-  to: 'designs',
-}, {
-  text: 'Contact Us',
-  to: 'contact-us',
-}
-]
-export default function UnderlineTabs() {
-  const [selectedIndex, setSelectedIndex] = useState(1);
+// const underlineTabs = [{
+//   text: 'Home',
+//   to: 'home',
+// }, {
+//   text: 'Designs & Apparel',
+//   to: 'designs',
+// }, {
+//   text: 'Contact Us',
+//   to: 'contact-us',
+// }
+// ]
+// export default function UnderlineTabs() {
+//   const [selectedIndex, setSelectedIndex] = useState(1);
  
-  const tabsComponents = underlineTabs.map((tab, i) => {
-    return (
-      <ScrollLink
-        type="button"
-        key={`tab-${tab.text}`}
-        onClick={() => setSelectedIndex(i)}
-        to={`${tab.to}`} spy={true} smooth={true} duration={300} offset={-50}
-        style={{
-          padding: "0.65rem 0.75rem",
-          color: 'var(--textPrimary)',
-          backgroundColor: "rgba(238, 238, 238)",
-          border: 0,
-          cursor: "pointer",
-        }}
-      >
-        {tab.text}
+//   const tabsComponents = underlineTabs.map((tab, i) => {
+//     return (
+//       <ScrollLink
+//         type="button"
+//         key={`tab-${tab.text}`}
+//         onClick={() => setSelectedIndex(i)}
+//         to={`${tab.to}`} spy={true} smooth={true} duration={300} offset={-50}
+//         style={{
+//           padding: "0.65rem 0.75rem",
+//           color: 'var(--textPrimary)',
+//           backgroundColor: "rgba(238, 238, 238)",
+//           border: 0,
+//           cursor: "pointer",
+//         }}
+//       >
+//         {tab.text}
  
-        {selectedIndex === i && (
-          <div style={{ position: "relative", transform: "translateY(3px)" }}>
-            <MagicTabSelect
-              id="underline"
-              transition={{ type: "spring", bounce: 0.3 }}
-            >
-              <div
-                style={{
-                  width: "100%",
-                  height: "0.15rem",
-                  backgroundColor: "black",
-                  position: "absolute",
-                }}
-              />
-            </MagicTabSelect>
-          </div>
-        )}
-      </ScrollLink>
-    );
-  });
+//         {selectedIndex === i && (
+//           <div style={{ position: "relative", transform: "translateY(3px)" }}>
+//             <MagicTabSelect
+//               id="underline"
+//               transition={{ type: "spring", bounce: 0.3 }}
+//             >
+//               <div
+//                 style={{
+//                   width: "100%",
+//                   height: "0.15rem",
+//                   backgroundColor: "black",
+//                   position: "absolute",
+//                 }}
+//               />
+//             </MagicTabSelect>
+//           </div>
+//         )}
+//       </ScrollLink>
+//     );
+//   });
  
-  return <div style={{ display: "flex", gap: "0.5rem" }}>{tabsComponents}</div>;
-}
+//   return <div style={{ display: "flex", gap: "0.5rem" }}>{tabsComponents}</div>;
+// }
--- app/src/pages/designs.js
+++ app/src/pages/designs.js
@@ -2,43 +2,43 @@ import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-const HOST = process.env.HOST;
-const imagePath = HOST + '/images/sweaters'
-const images = [
-    `${imagePath}/betty-strongService-sweater.png`,
-    `${imagePath}/betty-strongService.png`,
-    `${imagePath}/relentless.png`,
-    `${imagePath}/relentless-sweater-white.png`,
-    `${imagePath}/betty-icy-sweater.png`,
-    `${imagePath}/betty-icy.png`,
-    `${imagePath}/cake-sweater.png`,
-    `${imagePath}/cake.png`,
-    `${imagePath}/betty-horn-sweater-white.png`,
-    `${imagePath}/betty-horn.png`,
-    `${imagePath}/betty-bearliving-sweater.png`,
-    `${imagePath}/betty-bearliving.png`,
-    `${imagePath}/betty-relentless-sweater-white.png`,
-    `${imagePath}/betty-relentless.png`
-]
-
 export function Designs() {
 
     return (
         <>
         <div id="designs" >
         <Row style={{ marginTop: '10%', padding: '20px'}}>
-            <Col xs={4}>
+            <Col>
                 <h1 style={{ display: 'block', fontSize: '50px'}}>
                     Designs and Apparel
                 </h1>
-                <p style={{ display: 'block', fontSize: '25px'}}>Dive into our first collection of designs that seek to show our brand through apparel. All of the following are available in a sweater or sticker and are completely customizable upon request</p>
+                <p style={{ display: 'block', fontSize: '25px'}}>Dive into our first collection of designs that seek to show our brand through apparel. These sweaters and/or stickers are completely customizable upon request</p>
             </Col>
-            <Col xs={8} style={{ 
+            <Col style={{ 
                 borderRadius:'4px', 
                 backgroundColor: 'var(--background)',
                 }}>
-                <Row xs={8}>
-                    <Col d-flex justify-content-center align-items-center
+                    <Image
+                        style={{ 
+                            width:'350px', 
+                            height:'300px', 
+                            padding: '10px', 
+                            boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle)',
+                            transition: 'all 0.3s ease, filter 0.1s ease', // Add a transition for all properties over 0.3 seconds with ease timing function
+                            maxWidth: '100%',
+                            maxHeight: '100%',
+                        }}                                                                                            
+                        src='http://localhost:3000/images/sweaters/betty-bearliving-sweater.png'
+                        onMouseEnter={e => {
+                            const imageElement = e.currentTarget; 
+                            imageElement.src = "http://localhost:3000/images/sweaters/betty-bearliving.png";
+                        }}
+                        onMouseLeave={e => {
+                            e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-bearliving-sweater.png";
+                        }}
+                        />
+            </Col>
+            <Col d-flex justify-content-center align-items-center
                     style={{ width: '350px', height: '300px' }}>
                     <Image
                         style={{ 
@@ -50,41 +50,35 @@ export function Designs() {
                             maxWidth: '100%',
                             maxHeight: '100%',
                         }}                                                                                            
-                        src={images[0]}
+                        src='http://localhost:3000/images/sweaters/betty-icy-sweater.png'
                         onMouseEnter={e => {
                             const imageElement = e.currentTarget; 
-                            imageElement.src = images[1];
+                            imageElement.src = 'http://localhost:3000/images/sweaters/betty-icy.png';
                         }}
                         onMouseLeave={e => {
-                            e.currentTarget.src = images[0]
+                            e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-icy-sweater.png";
                         }}
                         />
-                    </Col>
-                    <Col d-flex justify-content-center align-items-center
-                    style={{ width: '350px', height: '300px' }}>
-                    <Image
+                </Col>
+            </Row>
+            <Row>
+                <Col style={{ /*width: '100%'*/}}>
+                        <Image
                         style={{ 
-                            width:'350px', 
-                            height:'300px', 
-                            padding: '10px', 
-                            boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle)',
-                            transition: 'all 0.3s ease, filter 0.1s ease', // Add a transition for all properties over 0.3 seconds with ease timing function
-                            maxWidth: '100%',
-                            maxHeight: '100%',
-                        }}                                                                                            
-                        src={images[2]}
-                        onMouseEnter={e => {
-                            const imageElement = e.currentTarget; 
-                            imageElement.src = images[3];
-                        }}
-                        onMouseLeave={e => {
-                            e.currentTarget.src = images[2];
-                        }}
-                        />
-                    </Col>
-                </Row>
-                <Row xs={4}>
-                    <Col d-flex justify-content-center align-items-center style={{ width: '100%'}}>
+                            width:'100%',
+                            padding: '30px',
+                            boxShadow: '10px 10px 10px var(--bs-dark-border-subtle)' 
+                            }}
+                            src='http://localhost:3000/images/sweaters/relentless-sweater-white.png'
+                            onMouseEnter={e => {
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/relentless.png";
+                            }}
+                            onMouseLeave={e => {
+                                e.currentTarget.src = 'http://localhost:3000/images/sweaters/relentless-sweater-white.png';
+                            }}
+                            />
+                </Col>
+                <Col d-flex justify-content-center align-items-center style={{ /*width: '100%'*/}}>
                         <Image
                         style={{ 
                             width:'400px',
@@ -92,18 +86,16 @@ export function Designs() {
                             padding: '30px',
                             boxShadow: '10px 10px 10px var(--bs-dark-border-subtle)' 
                             }}
-                            src={images[4]}
+                            src='http://localhost:3000/images/sweaters/betty-horn-sweater-white.png'
                             onMouseEnter={e => {
-                                e.currentTarget.src = images[5];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-horn.png";
                             }}
                             onMouseLeave={e => {
                                 
-                                e.currentTarget.src = images[4];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-horn-sweater-white.png";
                             }}
                             />
-                    </Col>
-                </Row>
-            </Col>
+                </Col>
         </Row>
         <Row>
             <Col xs={4} style={{ display: 'flex', alignContent: 'center', flexWrap: 'wrap'}}>
@@ -113,22 +105,15 @@ export function Designs() {
                         padding: '10px',
                         boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle)' 
                         }}
-                        src={images[6]}
+                        src='http://localhost:3000/images/sweaters/betty-strongService-sweater.png'
                         onMouseEnter={e => {
-                                e.currentTarget.src = images[7];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-strongService.png";
                             }}
                             onMouseLeave={e => {
-                                e.currentTarget.src = images[6];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/betty-strongService-sweater.png";
                         }}/>
             </Col>
-            <Col xs={8} 
-                style={{ 
-                    borderRadius:'4px', 
-                    //backgroundColor: 'var(--background)',
-                    //boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle' 
-                }}>
-                <Row xs={8}>
-                    <Col style={{ 
+            <Col xs={8} style={{ 
                         display: 'inline-grid',
                         alignContent: 'center',
                         alignItems: 'flex-end'
@@ -140,51 +125,15 @@ export function Designs() {
                             padding: '10px',
                             boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle)' 
                             }}
-                            src={images[8]}
+                            src='http://localhost:3000/images/sweaters/cake-sweater.png'
                             onMouseEnter={e => {
-                                e.currentTarget.src = images[9];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/cake-sticker.png";
                             }}
                             onMouseLeave={e => {
-                                e.currentTarget.src = images[8];
+                                e.currentTarget.src = "http://localhost:3000/images/sweaters/cake-sweater.png";
                             }}
                         />
-                    </Col>
-                    <Col>
-                        <Image
-                            style={{ 
-                            width:'100%',
-                            padding: '10px',
-                            boxShadow: '-10px 10px 10px var(--bs-dark-border-subtle)' 
-                            }}
-                            src={images[10]}
-                            onMouseEnter={e => {
-                                e.currentTarget.src = images[11];
-                            }}
-                            onMouseLeave={e => {
-                                e.currentTarget.src = images[10];
-                            }}
-                            />
-                    </Col>
-                </Row>
-                <Row xs={4}>
-                    <Col style={{ width: '100%'}}>
-                        <Image
-                        style={{ 
-                            width:'100%',
-                            padding: '30px',
-                            boxShadow: '10px 10px 10px var(--bs-dark-border-subtle)' 
-                            }}
-                            src={images[12]}
-                            onMouseEnter={e => {
-                                e.currentTarget.src = images[13];
-                            }}
-                            onMouseLeave={e => {
-                                e.currentTarget.src = images[12];
-                            }}
-                            />
-                    </Col>
-                </Row>
-            </Col>
+                </Col>
         </Row>
         </div>
         </>
