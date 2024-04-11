f1817ebf2e7c26334412131faf192a31e13b3632
isabel.hernandez <isabel.hernandez@omron.com>
2024-04-10 20:56:52 -0700

    fix for phone view

--- app/src/pages/posters.js
+++ app/src/pages/posters.js
@@ -8,7 +8,7 @@ export default function Posters({}) {
         <>
         <Row id="posters" style={{ justifyContent: 'center'}}>
             <img src={postersPath} style={{ 
-                    width: '50%', height: '50%',
+                    width: '80%', height: '80%',
                     boxShadow: '5px 5px 5px 2px rgb(190, 187, 187, 0.5)',
                     backgroundColor: 'rgb(72, 88, 14, 0.1)',
             }}/>
