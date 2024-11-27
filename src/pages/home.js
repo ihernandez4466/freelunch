ddf50c283cf34ce92238adf5cec4a744dff14014
Isabel Hernandez <ihernandez44@csuchico.edu>
2024-11-27 10:51:52 -0800

    Revert "add header?"
    This reverts commit 695e4c9c417ea85572818b5f45597fcc3af3b412.

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,7 @@ export default function Home(props) {
   return (
       <div id="home">
         <CustomNavBar {...props}/>
-        <Header />
+        {/* <Header /> */}
         <Sweaters {...props}/>
         <hr style={{ margin: '5rem 0'}}></hr>
         <Posters />        
@@ -22,29 +22,11 @@ export default function Home(props) {
   );
 }
 
-const textStyle = {
-  position: 'absolute',
-  inset: 0,
-  display: 'grid',
-  placeItems: 'center',
-  backgroundColor: '#000',
-  color: '#fff',
-  fontWeight: 900,
-  fontSize: '100px',
-  fontFamily: 'sans-serif',
-  mixBlendMode: 'multiply',
-  userSelect: 'none',
-};
-
 function Header() {
+  const headerPath = imagePath + '/branding/logo-icon.png';
   return (
     <Container fluid style={{ justifyContent: 'center', display: 'flex', background: 'linear-gradient(var(--background) 40%, white 18%, var(--primary) 70%)'}} id="home">    
-      <div style={{ position: 'relative', width: '100vw', height: '100vh'}}>
-        <video style={{width: '100%', height: '100%'}} autoPlay loop muted>
-          <source src="/videos/vineyards.mp4" type="video/mp4" />
-        </video>
-        <p style={textStyle}  >FREE LUNCH</p>
-      </div>
+        <img src={headerPath} style={{ padding: '15px', width: '35%', height: '20vw', WebkitMaskImage: 'linear-gradient(transparent 0%, black 40%, transparent 90%)'}}></img>
     </Container>
     );
   }
