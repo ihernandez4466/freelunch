b7cb48f2ee7e41582a2c5ae08cde3a6f427431dc
Isabel Hernandez <ihernandez44@csuchico.edu>
2024-12-02 14:32:15 -0800

    cookie start

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../public/styles/styles.css';
 import Head from 'next/head';
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from 'react';
+import { getCookie, setCookieInBrowser, checkExpiration, deleteCookie } from '../components/useCookie';
 
  
 // This default export is required in a new `pages/_app.js` file.
 export default function Home({ Component, pageProps }) {
+  const [cookie, setCookie] = useState(null);
+
+  useEffect(() => setUser(), [])
+
+  const setUser = () => {
+    let currentCookie = getCookie('userId');
+    if(currentCookie) {
+      // cookie already exists verify expiration
+      const expirationDate = new Date(cookieValue); // Assuming the value stores a valid date string.
+      const expired = checkExpiration(expirationDate)
+      if(expired){
+        deleteCookie('userId')
+      } else {
+        // insert into db or get from db
+
+      }
+    } else {
+      const newUuid = uuidv4();
+      let cookie = setCookieInBrowser('userId', newUuid)
+      setCookie(cookie)
+    }
+    // - if cookie exists: setUsername
+    // - if cookie is expired
+    //     - delete cookie
+    // - if cookie is not expired
+    //     - set username
+  }
+
   return (
   <>
     <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
-    <Component {...pageProps} userId={1}/>
+    <Component {...pageProps} userId={cookie}/>
   </>
 )}
