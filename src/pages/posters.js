c36879e8f3a30386ab536a213dcc2848a00145b9
Isabel Hernandez <ihernandez44@csuchico.edu>
2025-04-16 22:14:24 -0700

    add poster tease

--- src/pages/posters.js
+++ src/pages/posters.js
@@ -1,26 +1,37 @@
 import { useState } from 'react';
-import Row from 'react-bootstrap/Row';
+import { Col, Image } from 'react-bootstrap';
 
 /**
  * Functional component that renders page for posters (currently only a single image but should be broken up into widgets).
  */
 
 export default function Posters() {
-    const [error, setError] = useState(null)
-    const [data, setData] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-
     return(
         <div id="posters" style={{
-            alignContent: 'center',
-            alignItems: 'center',
+            padding: '30px', 
+            border: '2px solid var(--primary-transparent)', 
+            borderRadius: '20px 20px 20px 20px', 
+            display: 'flex', 
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
             {/* <Row>
                     <h1>
                         Posters
                     </h1>
             </Row> */}
-            <Row><div style={{ padding: '30px', backgroundColor: 'var(--primary-transparent)', borderRadius: '20px 20px 20px 20px', display: 'flex', justifyContent: 'center'}}><h2>Posters Coming Soon</h2></div></Row>
+            <Col sm={6} md={4}><div><h1>Posters Coming Soon</h1></div></Col>
+            <Col sm={6} md={8}>
+            <Image                 
+                style={{
+                    height: 'auto',
+                    padding: '10px',
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                }}
+                src="/images/branding/poster_tease.png"
+            />
+            </Col>
         </div>
     );
 }
