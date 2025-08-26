3dc1c17661df26325b81cb8dea69c4f06226a743
Isabel Hernandez <isabel.hernandez@greenhouse.io>
2025-08-25 21:56:10 -0700

    responsive for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ export default function Header({ imagePath }) {
           </div>
           <div id="brand" className="brand-section" style={{ 
             width: '100%', 
-            backgroundColor: 'var(--background)',
+            backgroundColor: 'var(--background-grey)',
             display: 'flex', 
             justifyContent: 'center', 
             alignItems: 'center',
